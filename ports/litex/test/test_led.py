3cb7f64e0215937d9219d1ecc82167f95e248b10
Florent Kermarrec <florent@enjoy-digital.fr>
2022-01-18 10:06:40 +0100

    ports/litex/test/test_led: Review/Cleanup, allow user to clearly see the Led toggling.

--- ports/litex/test/test_led.py
+++ ports/litex/test/test_led.py
@@ -1,10 +1,21 @@
-# Simple LED demo using litex module
+# Led test.
+# Copyright (C) 2021 Victor Suarez Rovere <suarezvictor@gmail.com>
+#
 # SPDX-License-Identifier: BSD-2-Clause
 
+import utime
 from litex import LED
-led = LED(0)
-led.on()
-led.off()
-led.toggle()
-led.toggle()
 
+# Create Led.
+led = LED(0)
+
+# Set Led On.
+led.on()
+
+# Toggle Led every 200ms.
+for i in range(8):
+	utime.sleep(0.2)
+	led.toggle()
+
+# Set Led Off.
+led.off()
