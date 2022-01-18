170fa2f64b70282682cde4cba5e2feb93d5458b2
Florent Kermarrec <florent@enjoy-digital.fr>
2022-01-18 09:52:04 +0100

    ports/litex/test/test_machine: Review/Cleanup.

--- ports/litex/test/test_machine.py
+++ ports/litex/test/test_machine.py
@@ -1,7 +1,17 @@
+# Machine test.
+# Copyright (C) 2021 Victor Suarez Rovere <suarezvictor@gmail.com>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
 import gc
 import umachine
-print("Machine identifier:", umachine.identifier())
-print("Clock frequency:", umachine.freq()/1.e6, "MHz")
-gc.collect()
-print("Free memory:", gc.mem_free()/1024, "KiB")
 
+# Display LiteX SoC identifier.
+print(umachine.identifier().decode("utf-8"))
+
+# Display sys_clk_freq.
+print("sys_clk_freq: {}MHz".format(umachine.freq()/1e6))
+
+# Display free memory.
+gc.collect()
+print("free memory: {}KiB".format(gc.mem_free()/1024))
