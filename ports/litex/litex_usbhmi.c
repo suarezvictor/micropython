c3f16d865c3699db1799e809a38d657853b9857f
Victor Suarez Rovere <suarezvictor@gmail.com>
2022-07-23 20:28:54 -0300

    added getters to usb hmi and made GIPO pin pairs configurable

--- ports/litex/litex_usbhmi.c
+++ ports/litex/litex_usbhmi.c
@@ -44,6 +44,19 @@ void hal_timer_setup(timer_idx_t timer_num, uint32_t usec, timer_isr_t handler)
 #include "py/obj.h"
 
 
+typedef struct _litex_usbhmi_type_t {
+    mp_obj_base_t base;
+    int pins[4];
+    int mousex, mousey, dx, dy, buttons, wheel;
+    uint64_t t0;
+} litex_usbhmi_obj_t;
+
+
+#define MP_GETTER_INT_IMPL(cls, typ, member) \
+STATIC mp_obj_t cls##_get_##member(mp_obj_t self_in) { \
+    return typ(((cls##_obj_t *) self_in)->member); } \
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(cls##_##member##_obj, cls##_get_##member);
+
 static unsigned activity_count = 0;
 void usbh_on_activitystatus(int on_off)
 {
@@ -56,7 +69,6 @@ void usbh_on_activitystatus(int on_off)
 
 void usbh_on_hiddata_log(uint8_t usbNum, uint8_t byte_depth, uint8_t* data, uint8_t data_len)
 {
-  // if( myListenUSBPort != usbNum ) return;
   printf("USB %d in (HID type %d): ", usbNum, hid_types[usbNum]);
   for(int k=0;k<data_len;k++) {
     printf("0x%02x ", data[k] );
@@ -96,18 +108,10 @@ void usbh_on_detect( uint8_t usbNum, void * dev )
 }
 
 
-typedef struct _litex_usbhmi_type_t {
-    mp_obj_base_t base;
-    int pins[4];
-    int mousex, mousey, dx, dy, buttons, wheel;
-    uint64_t t0;
-    mp_uint_t num;
-} litex_usbhmi_obj_t;
-
 const mp_obj_type_t litex_usbhmi_type;
 
 
-void usbhmi_initialize(litex_usbhmi_obj_t *self)
+void usbhmi_start(litex_usbhmi_obj_t *self)
 {
     printf("pins %d, %d, %d, %d!\n", self->pins[0], self->pins[1], self->pins[2], self->pins[3]);
     usbh_pins_init(self->pins[0], self->pins[1], self->pins[2], self->pins[3], USBH_QUEUE_SIZE);
@@ -161,7 +165,7 @@ int usbh_on_hidevent_mouse(int dx, int dy, int buttons, int wheel)
   mouse_event.buttons = buttons;
   mouse_event.wheel = wheel;
   
-  printf("MOUSE event: x %d (%+d), y %d (%+d), buttons 0x%02X wheel %d\n", mousex, dx, mousey, dy, buttons, wheel);
+  //printf("MOUSE event: x %d (%+d), y %d (%+d), buttons 0x%02X wheel %d\n", mousex, dx, mousey, dy, buttons, wheel);
   return true; //process all mouse events
 }
 
@@ -171,7 +175,7 @@ int usbh_on_hidevent_keyboard(uint8_t modifiers, uint8_t key, int pressed, char 
   return true;
 }
 
-void usbhmi_process(litex_usbhmi_obj_t *self)
+hid_protocol_t usbhmi_process(litex_usbhmi_obj_t *self)
 {
     //printf("Activity count %d\n", activity_count);
 	uint64_t t1 = micros();
@@ -180,37 +184,39 @@ void usbhmi_process(litex_usbhmi_obj_t *self)
 
     mousex = self->mousex;
     mousey = self->mousey;
-    usbh_hid_poll(dt); //may call events
-    self->mousex = mousex;
-    self->mousey = mousey;
-    self->dx = mouse_event.x;
-    self->dy = mouse_event.y;
-    self->buttons = mouse_event.buttons;
-    self->wheel = mouse_event.wheel;
+    hid_protocol_t proto = usbh_hid_poll(dt); //may call events
+    switch(proto)
+    {
+		case USB_HID_PROTO_MOUSE:
+		    self->mousex = mousex;
+		    self->mousey = mousey;
+		    self->dx = mouse_event.x;
+		    self->dy = mouse_event.y;
+		    self->buttons = mouse_event.buttons;
+		    self->wheel = mouse_event.wheel;
+		    break;
+		case USB_HID_PROTO_KEYBOARD:
+			break;
+		case USB_HID_PROTO_NONE:
+			break;
+	}
+	return proto;
 }
 
 STATIC mp_obj_t litex_usbhmi_make_new(const mp_obj_type_t *type_in,
 		size_t n_args, size_t n_kw, const mp_obj_t *args) {
-    mp_arg_check_num(n_args, n_kw, 1, 2, false); //TODO: allow first argument to be ommited
+    mp_arg_check_num(n_args, n_kw, 2, 4, false);
 
-    mp_uint_t num = mp_obj_get_int(args[0]);
 
-    if (num != 0) {
+    if (n_args & 1) {
          nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_ValueError,
-			"not a argument number: %d", num));
+			"pin numbers should be set in pairs, but an even number of arguments was passed"));
     }
     litex_usbhmi_obj_t *self = m_new_obj(litex_usbhmi_obj_t);
     self->base.type = &litex_usbhmi_type;
-    self->num = num;
 
-	static const int DP_P0 = 13; //D+
-	static const int DM_P0 = 12; //D-
-	static const int DP_P1 = 14; //D+
-	static const int DM_P1 = 15; //D-
-    self->pins[0] = DP_P0;
-    self->pins[1] = DM_P0;
-    self->pins[2] = DP_P1;
-    self->pins[3] = DM_P1;
+	for(int i= 0; i < n_args; ++i) //set pins from argument pairs
+      self->pins[i] = mp_obj_get_int(args[i]);
 
     return MP_OBJ_FROM_PTR(self);
 }
@@ -218,36 +224,48 @@ STATIC mp_obj_t litex_usbhmi_make_new(const mp_obj_type_t *type_in,
 void litex_usbhmi_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind)
 {
     litex_usbhmi_obj_t *self = (litex_usbhmi_obj_t *) self_in;
-    mp_printf(print, "usbhmi(%u):", self->num);
+    mp_printf(print, "USB_HMI, pins D0+:%d,D0-:%d,D1+:%d,D1-:%d", self->pins[0], self->pins[1], self->pins[2], self->pins[3]);
 }
 
-STATIC mp_obj_t litex_usbhmi_initialize(mp_obj_t self_in)
+STATIC mp_obj_t litex_usbhmi_start(mp_obj_t self_in)
 {
     litex_usbhmi_obj_t *self = (litex_usbhmi_obj_t *) self_in;
-    usbhmi_initialize(self);
+    usbhmi_start(self);
     return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(litex_usbhmi_initialize_obj, litex_usbhmi_initialize);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(litex_usbhmi_start_obj, litex_usbhmi_start);
 
 STATIC mp_obj_t litex_usbhmi_process(mp_obj_t self_in)
 {
     litex_usbhmi_obj_t *self = (litex_usbhmi_obj_t *) self_in;
-    usbhmi_process(self);
-    return mp_const_none;
+    return MP_OBJ_NEW_SMALL_INT(usbhmi_process(self));
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(litex_usbhmi_process_obj, litex_usbhmi_process);
 
+MP_GETTER_INT_IMPL(litex_usbhmi, MP_OBJ_NEW_SMALL_INT, mousex)
+MP_GETTER_INT_IMPL(litex_usbhmi, MP_OBJ_NEW_SMALL_INT, mousey)
+MP_GETTER_INT_IMPL(litex_usbhmi, MP_OBJ_NEW_SMALL_INT, dx)
+MP_GETTER_INT_IMPL(litex_usbhmi, MP_OBJ_NEW_SMALL_INT, dy)
+MP_GETTER_INT_IMPL(litex_usbhmi, MP_OBJ_NEW_SMALL_INT, buttons)
+MP_GETTER_INT_IMPL(litex_usbhmi, MP_OBJ_NEW_SMALL_INT, wheel)
 
 STATIC const mp_map_elem_t litex_usbhmi_locals_dict_table[] = {
-	{ MP_OBJ_NEW_QSTR(MP_QSTR_initialize), (mp_obj_t) &litex_usbhmi_initialize_obj },
-	{ MP_OBJ_NEW_QSTR(MP_QSTR_process), (mp_obj_t) &litex_usbhmi_process_obj },
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_start),	(mp_obj_t) &litex_usbhmi_start_obj },
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_process),		(mp_obj_t) &litex_usbhmi_process_obj },
+	//getters
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_mousex),	(mp_obj_t) &litex_usbhmi_mousex_obj },
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_mousey),	(mp_obj_t) &litex_usbhmi_mousey_obj },
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_dx),      (mp_obj_t) &litex_usbhmi_dx_obj },
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_dy),		(mp_obj_t) &litex_usbhmi_dy_obj },
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_buttons),	(mp_obj_t) &litex_usbhmi_buttons_obj },
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_wheel),	(mp_obj_t) &litex_usbhmi_wheel_obj },
 };
 
 STATIC MP_DEFINE_CONST_DICT(litex_usbhmi_locals_dict, litex_usbhmi_locals_dict_table);
 
 const mp_obj_type_t litex_usbhmi_type = {
 	{ &mp_type_type },
-	.name = MP_QSTR_USBHMI,
+	.name = MP_QSTR_USB_HMI,
 	.print = litex_usbhmi_print,
 	.make_new = litex_usbhmi_make_new,
 	.locals_dict = (mp_obj_t)&litex_usbhmi_locals_dict,
