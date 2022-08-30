3f0439caebcb13af67b2e932bd0a2fb05e546196
Victor Suarez Rovere <suarezvictor@gmail.com>
2022-08-30 20:43:22 -0300

    basic POC for imgui+micropython integration

--- ports/litex/dpg_lite/moddpg_lite.c
+++ ports/litex/dpg_lite/moddpg_lite.c
@@ -1,43 +1,18 @@
 // Copyright (C) 2021-2022 Victor Suarez Rovere <suarezvictor@gmail.com>
 // License: BSD-2-Clause
 
-
-/*
-https://dearpygui.readthedocs.io/en/latest/tutorials/first-steps.html
-
-import dearpygui.dearpygui as dpg
-
-dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=600, height=300)
-
-with dpg.window(label="Example Window"):
-    dpg.add_text("Hello, world")
-    dpg.add_button(label="Save")
-    dpg.add_input_text(label="string", default_value="Quick brown fox")
-    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
-
-dpg.setup_dearpygui()
-dpg.show_viewport()
-dpg.start_dearpygui()
-dpg.destroy_context()
-
-WITH example (with statement calls __enter__ and __exit__):
-class Demo:
-    def __enter__(self):
-        print(" calling to __enter__ method")
-        return "True"
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        print(" calling to __exit__ method")
-*/
-
 #include "py/obj.h"
 #include "py/objstr.h"
+
+#include <stdio.h> //printf
+void dpg_test_ui(float dt);
 
 STATIC mp_obj_t add_text(mp_obj_t arg) {
 
     GET_STR_DATA_LEN(arg, s, len);
     printf("add_text: %.*s\n", len, s);
+    for(;;)
+		dpg_test_ui(1./60);
     return mp_const_none;
 }
 MP_DEFINE_CONST_FUN_OBJ_1(add_text_obj, add_text);
