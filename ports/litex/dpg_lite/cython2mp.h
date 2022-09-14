cb2927df6774eb1ed408a1651d0b89816e2e7d5e
Victor Suarez Rovere <suarezvictor@gmail.com>
2022-09-13 21:45:59 -0300

    experimental cython-like layer for generated sources from pyimgui, tested with input_text

---
+++ ports/litex/dpg_lite/cython2mp.h
@@ -0,0 +1,52 @@
+#include <string.h>
+extern "C"
+{
+#include "py/obj.h"
+#include "py/objstr.h"
+#include "py/gc.h"
+#include "py/objtuple.h"
+}
+
+#define malloc(x) gc_alloc(x, false)
+#define free(x) gc_free(x)
+
+
+#define CYTHON_UNUSED
+#define __PYX_ERR(...) {}
+#define unlikely(x) (x)
+#define __Pyx_GOTREF(...)
+#define __Pyx_GIVEREF(...)
+#define __Pyx_XDECREF(...)
+#define __Pyx_XGIVEREF(...)
+#define __Pyx_INCREF(x)
+#define __Pyx_DECREF(x)
+#define __Pyx_AddTraceback(...)
+#define __Pyx_RefNannySetupContext(fn, i)
+#define __Pyx_RefNannyFinishContext()
+#define __Pyx_RefNannyDeclarations
+#define __Pyx_TraceDeclarations
+#define __Pyx_TraceFrameInit(x) {}
+#define __Pyx_TraceCall(...)
+#define __Pyx_TraceLine(...) {}
+#define __Pyx_TraceReturn(...)
+#define __Pyx_CppExn2PyErr()
+#define PyErr_Occurred() false
+#define PyErr_SetString(...)
+#define Py_None mp_const_none
+
+#define PyObject void //mp_obj_t is void *
+
+#define __Pyx_PyBool_FromLong(x) mp_obj_new_bool(x)
+#define PyTuple_New(n) mp_obj_new_tuple(n, NULL)
+#define PyTuple_SET_ITEM(t, i, v) ((mp_obj_tuple_t *)t)->items[i]=(v)
+
+static inline const char *__Pyx_PyBytes_AsString(mp_obj_t value)
+{
+  GET_STR_DATA_LEN(value, s_value, value_len);
+  return (const char *) s_value;
+}
+
+static inline mp_obj_t __Pyx_PyBytes_FromString(const char *str)
+{
+  return mp_obj_new_str(str, strlen(str));
+}
