dbb0dd17f78188ba6e39355cb851dedacec6c76d
Victor Suarez Rovere <suarezvictor@gmail.com>
2024-01-04 11:37:53 -0300

    add support for software-only renderers

--- ports/litex/accel_cores.c
+++ ports/litex/accel_cores.c
@@ -1,13 +1,17 @@
+#ifndef DISABLE_HARDWARE_ACCEL
 #include <liblitesdk/csr_helpers.h>
 #define ACCEL_STATIC_ASSERT(cond, msg) STATIC_ASSERT(cond, msg);
 #include <../../../accel_cores.h>
+#else
+#define VIDEO_FRAMEBUFFER_DEPTH 32 //assumes 32-bit color
+#endif
 
 #include "accel_cores.h"
 
 int accel_rectangle_fill32(uintptr_t fb_base, int x0, int y0, int x1, int y1, uint32_t rgba, unsigned frame_pitch)
 {
+#ifndef DISABLE_HARDWARE_ACCEL
   accel_rectangle_fill32_layout_t *regs = accel_rectangle_fill32_regs;
-
 #ifndef ACCEL_RECTANGLE_FILL32_CSR_PAGE_OFFSET
 #error MICROPY_ENABLE_FRAMEBUFFER_ACCEL is defined but ACCEL_RECTANGLE_FILL32_CSR_PAGE_OFFSET is not defined
 #endif
@@ -19,6 +23,10 @@ int accel_rectangle_fill32(uintptr_t fb_base, int x0, int y0, int x1, int y1, ui
 
   regs->run = 0; //stps
   while(regs->done); //wait data latch
+#else
+  accel_rectangle_fill32_layout_t local_regs;
+  accel_rectangle_fill32_layout_t *regs = &local_regs;
+#endif
 
   fb_base += (y0<y1?y0:y1)*frame_pitch + (x0<x1?x0:x1)*sizeof(rgba);
   regs->x0 = x0 < x1 ? x0 : x1;
@@ -30,16 +38,20 @@ int accel_rectangle_fill32(uintptr_t fb_base, int x0, int y0, int x1, int y1, ui
   regs->ystride = frame_pitch;
   regs->rgba = rgba;
 
+#ifndef DISABLE_HARDWARE_ACCEL
   regs->run = 1; //start
   while(!regs->done); //wait until done
+#else
+  rectangle_fill32(BUSMASTER_ARG, regs->x0, regs->x1, regs->y0, regs->y1, rgba, fb_base, regs->xstride, regs->ystride);
+#endif
   
   return 1; //executed
 }
 
 int accel_ellipse_fill32(uintptr_t fb_base, int x0, int y0, int x1, int y1, uint32_t rgba, unsigned frame_pitch)
 {
+#ifndef DISABLE_HARDWARE_ACCEL
   accel_ellipse_fill32_layout_t *regs = accel_ellipse_fill32_regs;
-
 #ifndef ACCEL_ELLIPSE_FILL32_CSR_PAGE_OFFSET
 #error MICROPY_ENABLE_FRAMEBUFFER_ACCEL is defined but ACCEL_ELLIPSE_FILL32_CSR_PAGE_OFFSET is not defined
 #endif
@@ -51,6 +63,10 @@ int accel_ellipse_fill32(uintptr_t fb_base, int x0, int y0, int x1, int y1, uint
 
   regs->run = 0; //stops
   while(regs->done); //wait data latch
+#else
+  accel_ellipse_fill32_layout_t local_regs;
+  accel_ellipse_fill32_layout_t *regs = &local_regs;
+#endif
 
   fb_base += (y0<y1?y0:y1)*frame_pitch + (x0<x1?x0:x1)*sizeof(rgba);
 
@@ -63,16 +79,20 @@ int accel_ellipse_fill32(uintptr_t fb_base, int x0, int y0, int x1, int y1, uint
   regs->ystride = frame_pitch;
   regs->rgba = rgba;
   
+#ifndef DISABLE_HARDWARE_ACCEL
   regs->run = 1; //start
   while(!regs->done); //wait until done
+#else
+  ellipse_fill32(BUSMASTER_ARG, regs->x0, regs->x1, regs->y0, regs->y1, rgba, fb_base, regs->xstride, regs->ystride);
+#endif
   
   return 1; //executed
 }
 
 int accel_line32(uintptr_t fb_base, int x0, int y0, int x1, int y1, uint32_t rgba, unsigned frame_pitch)
 {
+#ifndef DISABLE_HARDWARE_ACCEL
   accel_line32_layout_t *regs = accel_line32_regs;
-
 #ifndef ACCEL_LINE32_CSR_PAGE_OFFSET
 #error MICROPY_ENABLE_FRAMEBUFFER_ACCEL is defined but ACCEL_LINE32_CSR_PAGE_OFFSET is not defined
 #endif
@@ -84,6 +104,10 @@ int accel_line32(uintptr_t fb_base, int x0, int y0, int x1, int y1, uint32_t rgb
 
   regs->run = 0; //stops
   while(regs->done); //wait data latch
+#else
+  accel_line32_layout_t local_regs;
+  accel_line32_layout_t *regs = &local_regs;
+#endif
 
   fb_base += y0*frame_pitch + x0*sizeof(rgba);
   regs->x0 = x0;
@@ -97,9 +121,12 @@ int accel_line32(uintptr_t fb_base, int x0, int y0, int x1, int y1, uint32_t rgb
 
   regs->rgba = rgba;
   
+#ifndef DISABLE_HARDWARE_ACCEL
   regs->run = 1; //start
   while(!regs->done); //wait until done
-  
+#else
+  line32(BUSMASTER_ARG, regs->x0, regs->x1, regs->y0, regs->y1, rgba, fb_base, regs->xstride, regs->ystride);
+#endif  
   return 1; //executed
 }
 
--- ports/litex/accel_cores.h
+++ ports/litex/accel_cores.h
@@ -7,8 +7,16 @@
 //each accelerator functions return a boolean to indicate if it was run or not, to resort to software renderer if not
 //(i.e it can check if the target buffer is within the allowable region)
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int accel_rectangle_fill32(uintptr_t fb_base, int x0, int y0, int x1, int y1, uint32_t rgba, unsigned frame_pitch);
 int accel_ellipse_fill32(uintptr_t fb_base, int x0, int y0, int x1, int y1, uint32_t rgba, unsigned frame_pitch);
 int accel_line32(uintptr_t fb_base, int x0, int y0, int x1, int y1, uint32_t rgba, unsigned frame_pitch);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //__ACCEL_CORES_H
