ec77ad0e743556bd5d6717a7f88f3e4cb2ad31e4
Victor Suarez Rovere <suarezvictor@gmail.com>
2024-01-25 12:15:49 -0300

    moved UART getc functions to driver

--- ports/f133/mphalport.h
+++ ports/f133/mphalport.h
@@ -7,54 +7,28 @@
 
 #include <stdbool.h>
 #include "sys.h" //CPU functions
-#include "ioregs.h"
-
-//FIXME: move to driver_uart.c
-static inline int driver_uart_wontblock(void)
-{
-  const intptr_t UART_BASE = 0x02500000;
-  const int UART_LSR = 0x0014;
-  const int UART_LSR_DR = 0x01; // Receiver data ready
-  return (io_read32(UART_BASE + UART_LSR) & UART_LSR_DR) != 0;
-}
-
-void driver_uart_putc(char c);
-static inline int driver_uart_getc(void)
-{
-  if(!driver_uart_wontblock())
-    return -1;
-
-  const intptr_t UART_BASE = 0x02500000;
-  const int UART_RBR = 0x0000;
-  return io_read32(UART_BASE + UART_RBR);
-}
-
+#include "driver_uart.h"
 
 static inline void mp_hal_set_interrupt_char(char c) {}
 
 // Send the string of given length.
-static inline void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) {
-    while (len--) {
+static inline void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len)
+{
+    while (len--)
        driver_uart_putc(*str++);
-    }
 }
 
 extern void mp_handle_pending(bool);
 #define MICROPY_EVENT_POLL_HOOK mp_handle_pending(true)
 
 // Receive single character, blocking until one is available.
-static inline int mp_hal_stdin_rx_chr(void) {
-    char c;
-
-    while(!driver_uart_wontblock())
+static inline int mp_hal_stdin_rx_chr(void)
+{
+    while(!driver_uart_getc_wontblock())
     	MICROPY_EVENT_POLL_HOOK;
 
-    c = driver_uart_getc();
-    // \n to \r conversion
-    if (c == '\n')
-        return '\r';
-    else
-        return c;
+    char c = driver_uart_getc();
+    return c == '\n' ? '\r' : c;
 }
 
 
